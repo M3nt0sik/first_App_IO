17158428193130da302f70135a14e91000f07173
Kamil <mentos.96@o2.pl>
2023-07-17 20:38:09 +0200

    end of excercise

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        using (var ms = new MemoryStream()) { 
+            using (var sw = new StreamWriter(ms))
+            {
+                sw.WriteLine("Wartosc wynosi {0:0.00}", 123.456789);
+                
+            }
+            Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+        }
+    }
+}
